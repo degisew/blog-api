ad431d14522b96c2b8c34c8f4827f4286a0b3111
Degisew <jegisew21@gmail.com>
2023-04-25 18:22:31 +0300

    Make fields more realistic

--- products/models.py
+++ products/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    name = models.TextField()
-    price = models.IntegerField()
-    description = models.TextField(default='My first product ever!')
+    name = models.CharField(max_length=20, blank=False)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    description = models.TextField()
