44ed31d0a51115e420e36952b988daadf4f6ee4a
Degisew <jegisew21@gmail.com>
2024-12-20 23:57:21 +0300

    initial commit

--- products/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Product(models.Model):
-    name = models.CharField(max_length=20, blank=False)
-    price = models.DecimalField(decimal_places=2, max_digits=10)
-    description = models.TextField()
