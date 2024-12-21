6f4f56620b6cb9d978c844bd3e2f31a2b109f44d
Degisew <jegisew21@gmail.com>
2024-12-21 15:16:08 +0300

    Add: user creation forms

--- blog/accounts/admin.py
+++ blog/accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserChangeForm
 
-# Register your models here.
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "username",
+        "name",
+        "is_staff",
+    ]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("name",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("name",)}),)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
